3b2be447728fb9e76a62c0d3ff2cb37c7ff7f561
Petr Kudrnovský <kudrnovsky.p@gmail.com>
2025-01-08 17:17:53 +0100

    Add appropriate name

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Petrova digitální zahrada",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
