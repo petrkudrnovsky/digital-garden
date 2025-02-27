a51512f2a69e91cef7b5c5e0c061a2e9df66f890
Petr Kudrnovský <kudrnovsky.p@gmail.com>
2025-02-27 17:30:10 +0100

    Quartz sync: Feb 27, 2025, 5:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Petrova digitální zahrada 🚀",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "cs-CZ",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
